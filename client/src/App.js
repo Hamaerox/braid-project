1b92891aad931f97f8349e14b3b555cab1751b3e
Hamaerox <timd1026@gmail.com>
2019-03-23 14:32:53 -0400

    project start

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import Home from './Home'
+
+const App = () => (
+    <div>
+        <p>Start of my new career right here</p>
+    </div>
+)
+
+export default App
