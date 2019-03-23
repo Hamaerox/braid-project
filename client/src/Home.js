59d4c0b3cfea5ee80f00d024621f9c198bd9c6b2
Hamaerox <timd1026@gmail.com>
2019-03-23 17:37:40 -0400

    async done

--- client/src/Home.js
+++ client/src/Home.js
@@ -10,25 +10,71 @@ class Home extends Component {
             kvbw: '',
             kshd: '',
             kgve: '',
-            kw13: '',
             kiad: ''
         }
     }
 
     
-     componentDidMount(){
-         axios.get(`https://wx.wearebraid.com/stations/KCHO?api_key=${process.env.REACT_APP_BRAID_KEY}`).then(res => {
-            this.setState({
-                
-            })    
-         console.log(res)
-         })
+    //  componentDidMount(){
+    //      axios.get(`/forwarder?url=https://wx.wearebraid.com/stations/KCHO?api_key=${process.env.REACT_APP_BRAID_KEY}`).then(res => {
+    //         this.setState({
+    //             kcho: res.data
+    //         })    
+    //         console.log(res.data['Wind-Speed'])
+    //      })
 
+    //      axios.get(`/forwarder?url=https://wx.wearebraid.com/stations/KVBW?api_key=${process.env.REACT_APP_BRAID_KEY}`).then(res => {
+    //         this.setState({
+    //             kvbw: res.data
+    //         }) 
+    //      })
+
+    //      axios.get(`/forwarder?url=https://wx.wearebraid.com/stations/KSHD?api_key=${process.env.REACT_APP_BRAID_KEY}`).then(res => {
+    //         this.setState({
+    //             kshd: res.data
+    //         })  
+    //      })
+
+    //      axios.get(`/forwarder?url=https://wx.wearebraid.com/stations/KGVE?api_key=${process.env.REACT_APP_BRAID_KEY}`).then(res => {
+    //         this.setState({
+    //             kgve: res.data
+    //         }) 
+    //      })
+    //         // BROEKN, NO DATA. CORS ERROR. FORWARDER DIDN'T WORK.
+    //     //  axios.get(`/forwarder?url=https://wx.wearebraid.com/stations/KW13?api_key=${process.env.REACT_APP_BRAID_KEY}`).then(res => {
+    //     //     this.setState({
+    //     //         kw13: res.data
+    //     //     })    
+    //     //  })
+
+    //      axios.get(`/forwarder?url=https://wx.wearebraid.com/stations/KIAD?api_key=${process.env.REACT_APP_BRAID_KEY}`).then(res => {
+    //         this.setState({
+    //             kiad: res.data
+    //         })  
+    //      })
+    //     }
+
+        async componentDidMount(){
+            const kcho = await axios.get(`/forwarder?url=https://wx.wearebraid.com/stations/KCHO?api_key=${process.env.REACT_APP_BRAID_KEY}`)
+            const kvbw = await axios.get(`/forwarder?url=https://wx.wearebraid.com/stations/KVBW?api_key=${process.env.REACT_APP_BRAID_KEY}`)
+            const kshd = await axios.get(`/forwarder?url=https://wx.wearebraid.com/stations/KSHD?api_key=${process.env.REACT_APP_BRAID_KEY}`)
+            const kgve = await axios.get(`/forwarder?url=https://wx.wearebraid.com/stations/KGVE?api_key=${process.env.REACT_APP_BRAID_KEY}`)
+            const kiad = await axios.get(`/forwarder?url=https://wx.wearebraid.com/stations/KIAD?api_key=${process.env.REACT_APP_BRAID_KEY}`)
+            const newObj = {
+                kcho: kcho.data,
+                kvbw: kvbw.data,
+                kshd: kshd.data,
+                kgve: kgve.data,
+                kiad: kiad.data
+            }
+            this.setState(newObj)
         }
 
 
     render(){
+        console.log(this.kcho)
         return(
+            
             <div className='stationContainer'>
                 <div className='charlottesville'>
                     <p>charlottesville</p>
@@ -43,7 +89,7 @@ class Home extends Component {
                             <p>gordonsville</p>
                         </div>
                     <div className='waynesboro'>
-                        <p>waynesboro</p>
+                        <p>Waynesboro system is currently down</p>
                     </div>
                 <div className='washington'>
                     <p>washington</p>
